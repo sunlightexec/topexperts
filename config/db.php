0a0248c3bc785be86aaa85ee93423b4203cd6136
a_druppov <a_druppov@sitegeneration.ru>
2018-10-09 17:38:43 +0300

    first commit

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=topexperts',
+    'dsn' => 'mysql:host=localhost;dbname=admin_topexperts',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
