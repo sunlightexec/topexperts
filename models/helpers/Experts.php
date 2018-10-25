5afe65da83d9bdf506d5ab4a3175e3072d42d225
a_druppov <a_druppov@sitegeneration.ru>
2018-10-25 20:01:28 +0300

    add queue

--- models/helpers/Experts.php
+++ models/helpers/Experts.php
@@ -133,8 +133,8 @@ class Experts extends \app\models\Experts
 //        print_r($updates);die();
 
         if(!empty($updatesFlip) || !empty($updatesHold)) {
-            $model->flip = $updatesFlip['flipCount']==0 ? 0 : round($updatesFlip['flipSum'] / $updatesFlip['flipCount'],1);
-            $model->hold = $updatesHold['holdCount']==0 ? 0 : round($updatesHold['holdSum'] / $updatesHold['holdCount'],1);
+            $model->flip = $updatesFlip['flipCount']==0 ? 0 : round($updatesFlip['flipSum'] / $updatesFlip['flipCount'],2);
+            $model->hold = $updatesHold['holdCount']==0 ? 0 : round($updatesHold['holdSum'] / $updatesHold['holdCount'],2);
             $model->save();
         } else {
             $model->flip = 0;
